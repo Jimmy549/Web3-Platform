bfd0edbafcaead821954daf68ffb48cb2a5a1ec9
Jimmy549 <ehmitservices1@gmail.com>
2026-01-14 12:26:46 +0500

    fix: MUI Grid v7 compatibility

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -88,7 +88,7 @@ export default function ProfilePage() {
           <Divider sx={{ my: 3, borderColor: '#73FDAA33' }} />
 
           <Grid container spacing={3}>
-            <Grid item xs={12}>
+            <Grid xs={12}>
               <Paper
                 sx={{
                   p: 3,
@@ -110,7 +110,7 @@ export default function ProfilePage() {
               </Paper>
             </Grid>
 
-            <Grid item xs={12}>
+            <Grid xs={12}>
               <Paper
                 sx={{
                   p: 3,
